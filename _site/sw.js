5b124d4c179de507921f899611c0e7cba7974b9d
Nasrul Aziz <aziznasrul85@gmail.com>
2022-11-28 16:22:41 +0700

    fix

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221124.002651';
+const cacheName = 'chirpy-20221128.161949';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
